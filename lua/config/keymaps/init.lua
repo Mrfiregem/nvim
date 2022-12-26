2bf217599323de1da37526413be2086fd2515c45
Firegem <mrfiregem@protonmail.ch>
2022-12-26 03:25:15 -0500

    Add 'smart-splits.nvim'

--- lua/config/keymaps/init.lua
+++ lua/config/keymaps/init.lua
@@ -1,6 +1,8 @@
 local wk = require("which-key")
 local tb = require("telescope.builtin")
+local sms = require("smart-splits")
 
+-- '<leader>' keybinds
 wk.register({
   [" "] = { "<cmd>nohlsearch<cr>", "Clear Highlight" },
   t = {
@@ -11,3 +13,24 @@ wk.register({
     g = { function() tb.live_grep() end, "Grep Files In PWD" },
   },
 }, { prefix = "<leader>" })
+
+-- Window navigation
+wk.register {
+  -- Resize windows
+  ["<a-h>"] = { sms.resize_left, "Resize split left" },
+  ["<a-j>"] = { sms.resize_down, "Resize split down" },
+  ["<a-k>"] = { sms.resize_up, "Resize split up" },
+  ["<a-l>"] = { sms.resize_right, "Resize split right" },
+  ["<a-=>"] = { "<c-w>=", "Eqully resize all splits" },
+  -- Navigate windows
+  ["<c-h>"] = { sms.move_cursor_left, "Navigate to split left" },
+  ["<c-j>"] = { sms.move_cursor_down, "Navigate to split down" },
+  ["<c-k>"] = { sms.move_cursor_up, "Navigate to split up" },
+  ["<c-l>"] = { sms.move_cursor_right, "Navigate to split right" },
+  -- Modify window layout
+  ["<a-H>"] = { "<c-w>H", "Move split left" },
+  ["<a-J>"] = { "<c-w>J", "Move split down" },
+  ["<a-K>"] = { "<c-w>K", "Move split up" },
+  ["<a-L>"] = { "<c-w>L", "Move split right" },
+  ["<a-c>"] = { "<cmd>close<cr>", "Close split window" },
+}
---
+++ lua/plugins/smart-splits.lua
@@ -0,0 +1,6 @@
+local M = {
+  "mrjones2014/smart-splits.nvim",
+  keys = { "<a-h>", "<a-j>", "<a-k>", "<a-l>", "<c-h>", "<c-j>", "<c-k>", "<c-l>" },
+}
+
+return M
