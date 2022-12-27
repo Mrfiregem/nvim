260ca4d3a1d370d8d7f8979264ab66c686ad0ba0
Firegem <mrfiregem@protonmail.ch>
2022-12-27 04:57:11 -0500

    Allow ':FormatWrite' command to be chainable

--- lua/lsp/on_attach.lua
+++ lua/lsp/on_attach.lua
@@ -3,9 +3,9 @@ local function format_write(bufnr)
   vim.api.nvim_buf_call(bufnr, function() vim.cmd("silent! write") end)
 end
 
-local F = function(client, bufnr)
+return function(_, bufnr) -- Takes client, buffer_number
+  -- Add keybindings if LSP is present
   require("config.keymaps.lsp")
-  vim.api.nvim_buf_create_user_command(bufnr, "FormatWrite", function() format_write(bufnr) end, {})
+  -- Add ':FormatWrite' command to buffers with LSP attached
+  vim.api.nvim_buf_create_user_command(bufnr, "FormatWrite", function() format_write(bufnr) end, { bar = true })
 end
-
-return F
