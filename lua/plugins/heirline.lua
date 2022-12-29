2f7ce4fb5f775522f73e26f246095f7c83e2d220
Firegem <mrfiregem@protonmail.ch>
2022-12-29 11:13:29 -0500

    todo: configure heirline

---
+++ lua/plugins/heirline.lua
@@ -0,0 +1,15 @@
+local M = {'rebelot/heirline.nvim'}
+
+-- Below each window
+local statusline = {
+}
+
+-- -- Top of each window
+-- local winbar = {
+-- }
+
+-- Top of all windows
+local tabline = {
+}
+
+return M
