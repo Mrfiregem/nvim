f4ee7c9ca894051a68edf06e80423c8a91fecc38
Firegem <mrfiregem@protonmail.ch>
2022-12-30 14:43:33 -0500

    change toggleterm key to '<C-z>'

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,15 +1,26 @@
+local toggle_key = "<c-z>"
+
 local M = {
   "akinsho/toggleterm.nvim",
-  keys = { [[<c-\>]] },
+  keys = { toggle_key },
   cmd = { "ToggleTerm", "ToggleTermToggleAll", "Lazygit" },
 }
 
 function M.config()
   -- Set configuration
-  require("toggleterm").setup { open_mapping = [[<c-\>]] }
+  require("toggleterm").setup { open_mapping = toggle_key }
 
   local Terminal = require("toggleterm.terminal").Terminal
-  local lazygit = Terminal:new { cmd = "lazygit", hidden = true, direction = "float", dir = "git_dir" }
+  local lazygit = Terminal:new {
+    cmd = "lazygit",
+    hidden = true,
+    direction = "float",
+    dir = "git_dir",
+    on_open = function(term)
+      vim.cmd.startinsert { bang = true }
+      vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<cr>", { noremap = true, silent = true })
+    end,
+  }
   vim.api.nvim_create_user_command("Lazygit", function()
     if require("utils.check").in_git_repo() then
       lazygit:toggle()
