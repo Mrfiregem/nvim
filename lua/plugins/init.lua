9b3e7dc6a54aead7ad2706225c8057f935e0e26a
Firegem <mrfiregem@protonmail.ch>
2022-12-30 14:17:34 -0500

    Install 'legendary' plugin organize plugins list into groups

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,30 +1,38 @@
 local uc = require("utils.check")
 return {
-  -- Declare keybinds and show possible keymaps on press
-  { "folke/which-key.nvim", lazy = true },
-
-  -- Configs for nvim's LSP client
-  { "neovim/nvim-lspconfig", event = "BufReadPost" },
-
-  -- Show LSP function signatures
-  { "ray-x/lsp_signature.nvim", lazy = true },
-
   -- Colorschemes
   { "catppuccin/nvim", name = "catppuccin", lazy = true },
   { "rebelot/kanagawa.nvim", lazy = true },
 
+  -- LSP-related plugins
+  -- : Configs for nvim's LSP client
+  { "neovim/nvim-lspconfig", event = "BufReadPost" },
+  -- : Show LSP function signatures
+  { "ray-x/lsp_signature.nvim", lazy = true },
+  -- : Configure LSP for neovim configuration
+  { "folke/neodev.nvim", config = true, cond = uc.in_config_dir },
+  -- : Better looking ui elements (not just LSP)
+  "stevearc/dressing.nvim",
+
+  -- Ease-of-use text plugins
+  -- : Comment out lines or blocks quickly
+  { "numToStr/Comment.nvim", config = true, keys = { "gcc", "gbc", "gc", "gb" } },
+  -- : Auto-close delimiter characters (e.g. ", ', <, etc.)
+  { "windwp/nvim-autopairs", config = {}, event = "BufReadPost" },
+
+  -- Keymaps and command configuration helpers
+  -- : Declare keybinds and show possible keymaps on press
+  { "folke/which-key.nvim", lazy = true },
+  {
+    "mrjones2014/legendary.nvim",
+    config = { which_key = { auto_register = true } },
+    keys = { { "<c-p>", "<cmd>Legendary<cr>", desc = "Show list of all commands tied to a keymap" } },
+    lazy = false,
+  },
+
   -- Git / version control
   { "lewis6991/gitsigns.nvim", config = true, cond = uc.in_git_repo, event = "BufReadPost" },
 
-  -- Comment out lines or blocks quickly
-  { "numToStr/Comment.nvim", config = true, keys = { "gcc", "gbc", "gc", "gb" } },
-
-  -- Configure LSP for neovim configuration
-  { "folke/neodev.nvim", config = true, cond = uc.in_config_dir },
-
   -- Better window navigation functions
   { "mrjones2014/smart-splits.nvim", event = "WinNew" },
-
-  -- Auto-close delimiter characters (e.g. ", ', <, etc.)
-  { "windwp/nvim-autopairs", config = {}, event = "BufReadPost" },
 }
