b94c1406f0fac8874fa979805f2b9c4964f09ad6
Firegem <mrfiregem@protonmail.ch>
2023-01-21 22:46:01 -0500

    treesitter incremental selection keymap

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,10 +7,22 @@ local M = {
 
 function M.config()
   require("nvim-treesitter.configs").setup {
+    -- List of language parsers to automatically install
     ensure_installed = require("utils.lists").ts_langs,
+    -- Enable modules
     highlight = { enable = true },
     indent = { enable = false },
     yati = { enable = true },
+    -- Select text based on treesitter node
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<CR>", -- Normal: Select node uder cursor
+        node_incremental = "<CR>", -- Visual: Grow selection to parent node
+        scope_incremental = "<C-CR>", -- Visual: Grow selection to parent scope
+        node_decremental = "<BS>", -- Visual: Shrink selection to child node
+      },
+    },
   }
 end
 
